bb61639d8b64d0f6e2ca658208a899b466d087e5
Duncan Mackay <duncanpmackay@outlook.com>
2022-01-28 11:24:34 +0000

    Project initiated

---
+++ puttr.app/Data/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace puttr.uk.app.Data
+{
+    static public class Class
+    {
+        public enum Type { working = 1 }
+
+    };
+
+}
---
+++ puttr.app/Data/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace puttr.uk.app.Data
+{
+    public class User
+    {
+        public int id;
+        public string handle;
+
+
+    }
+}
